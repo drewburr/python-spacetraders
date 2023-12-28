2422986dd8f1f59fb466e4c6a2a3f4577325efc5
drewburr <drew@drewburr.com>
2023-12-28 12:26:17 -0500

    Add generation script

---
+++ generate.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Generate a client from current API spec
+
+CLONE_DIR='./api-docs'
+
+poetry install --only dev
+
+if [ -d $CLONE_DIR ]
+then
+    echo "Pulling updates from SpaceTradersAPI/api-docs"
+    cd $CLONE_DIR
+    git fetch
+    git pull
+    cd -
+else
+    echo "Cloning SpaceTradersAPI/api-docs"
+    git clone https://github.com/SpaceTradersAPI/api-docs $CLONE_DIR
+fi
+
+cd $CLONE_DIR
+npx @redocly/openapi-cli@latest bundle -o ../bundle.json
+cd -
+
+poetry run openapi-python-client update \
+    --path bundle.json \
+    --meta none \
+    --config openapi-client.yml \
+    --custom-template-path=openapi-client-template
