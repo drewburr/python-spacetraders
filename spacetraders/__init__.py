e908cba986f399bc6a09f0ab1bce9530fca7f5f4
drewburr <drew@drewburr.com>
2024-01-14 21:37:34 -0500

    Commit current state

--- spacetraders/__init__.py
+++ spacetraders/__init__.py
@@ -1,9 +1,7 @@
 """ A client library for accessing SpaceTraders API """
-from .client import AsyncClient, AuthenticatedAsyncClient, AuthenticatedClient, Client
+from .client import AuthenticatedClient, Client
 
 __all__ = (
-    "AsyncClient",
-    "AuthenticatedAsyncClient",
     "AuthenticatedClient",
     "Client",
 )
