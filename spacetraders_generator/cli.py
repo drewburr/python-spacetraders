7c3d854883644f841b17a14c559cc22cea50a1db
drewburr <drewburr9@gmail.com>
2024-02-16 00:05:11 -0500

    Add openapi spec parsing

--- spacetraders_generator/cli.py
+++ spacetraders_generator/cli.py
@@ -1,5 +1,9 @@
 import json
 import typer
+from pathlib import Path
+from openapi_python_client import cli as oapi_cli
+from openapi_python_client import config as oapi_config
+import openapi_python_client as oapi_client
 
 app = typer.Typer()
 
@@ -7,7 +11,19 @@ app = typer.Typer()
 def generate(path: str):
     """Generate client from relative path"""
 
+    # Hack together openapi_python_client to parse OpenAPI spec
     with open(path) as f:
         bundle=json.load(f)
 
     print(bundle['openapi'])
+
+    configFile = oapi_config.ConfigFile.load_from_path(Path('openapi-client.yml'))
+
+    config = oapi_config.Config.from_sources(config_file=configFile, meta_type=oapi_cli.MetaType.NONE, document_source=Path('bundle.json'), file_encoding='utf-8')
+
+    project = oapi_client._get_project_for_url_or_path(config)
+
+    models = list(project.openapi.models)
+    endpoint_collections = project.openapi.endpoint_collections_by_tag
+
+    print(endpoint_collections.keys())
